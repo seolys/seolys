6fed8421f0ab527189a359fbedcbe12041c075a0
설 연수 <49857359+seolys@users.noreply.github.com>
2020-09-27 18:51:13 +0900

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -25,7 +25,7 @@ def replace_chunk(content, marker, chunk, inline=False):
 
 
 organization_graphql = """
-  organization(login: "dogsheep") {
+  organization(id: "seolnavy") {
     repositories(first: 100, privacy: PUBLIC) {
       pageInfo {
         hasNextPage
