06401203fa24efe06cecb6356062038570989140
seol <seolnavy@naver.com>
2020-09-27 19:27:03 +0900

    token change

--- main.go
+++ main.go
@@ -186,7 +186,7 @@ var markdownTableHeaderTmpl = `
 func main() {
 	// authenticate to github
 	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("SIMONW_TOKEN")},
+		&oauth2.Token{AccessToken: os.Getenv("README_TOKEN")},
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
 	client := githubv4.NewClient(httpClient)
